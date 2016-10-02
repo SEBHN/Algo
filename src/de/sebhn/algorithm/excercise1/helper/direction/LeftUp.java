f5651b59b9d629d9204ed11190a30f3d4e2638b7
Manuel <wtfjoke@livenet.ch>
2016-10-02 16:48:38 +0200

    fix horrible mistakes on leftup (works! :D)

--- src/de/sebhn/algorithm/excercise1/helper/direction/LeftUp.java
+++ src/de/sebhn/algorithm/excercise1/helper/direction/LeftUp.java
@@ -7,12 +7,12 @@ import de.sebhn.algorithm.excercise1.helper.Position;
 public class LeftUp implements Walkable {
 
   public static Position getNewPosition(Position currentPosition, Grid grid) {
-    Position newPosition = new Position(currentPosition.getX() - 1, currentPosition.getY() - 1);
+    Position newPosition = new Position(currentPosition.getX() - 1, currentPosition.getY() + 1);
     return newPosition;
   }
 
   public static boolean isAllowedToWalk(Position newPosition, Grid grid, Position currentPosition) {
-    return Diagonal.isAboveOrOn(newPosition) && Diagonal.isAboveOrOn(newPosition)
+    return Diagonal.isAboveOrOn(newPosition) && Diagonal.isAboveOrOn(currentPosition)
         && grid.isInGrid(newPosition);
   }
 
