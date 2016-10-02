b0721f5b7ba4a210ece4548d2c88b06010e0d1b7
Manuel <wtfjoke@livenet.ch>
2016-10-02 16:54:22 +0200

    rename

--- src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
+++ src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
@@ -1,10 +1,12 @@
 package de.sebhn.algorithm.excercise1.helper;
 
-public class HelperStarter {
+public class StarterHelper {
 
   public static void main(String[] args) {
+    long start = System.currentTimeMillis();
     Grid grid = new Grid(3);
     GraphMatrix matrix = GridVisitor.createPossibleEdges(grid);
     GridVisitor.findPath(grid, matrix);
+    System.out.println("duration: " + (System.currentTimeMillis() - start));
   }
 }
