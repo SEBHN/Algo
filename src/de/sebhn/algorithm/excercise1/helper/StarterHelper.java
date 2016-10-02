9233c647d18364cc5c6e4c891f8d21d53d960ced
Manuel <wtfjoke@livenet.ch>
2016-10-02 17:35:01 +0200

    show n when beginning

--- src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
+++ src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
@@ -4,7 +4,9 @@ public class StarterHelper {
 
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
-    Grid grid = new Grid(3);
+    int n = 3;
+    Grid grid = new Grid(n);
+    System.out.println("n=" + n);
     GraphMatrix matrix = GridVisitor.createPossibleEdges(grid);
     GridVisitor.findPath(grid, matrix);
     System.out.println("duration: " + (System.currentTimeMillis() - start));
