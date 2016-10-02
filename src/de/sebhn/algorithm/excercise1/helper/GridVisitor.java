62b01187eb2ae68782ba6179eb68f1148d96eef2
Manuel <wtfjoke@livenet.ch>
2016-10-02 17:07:15 +0200

    add hint for showing paths

--- src/de/sebhn/algorithm/excercise1/helper/GridVisitor.java
+++ src/de/sebhn/algorithm/excercise1/helper/GridVisitor.java
@@ -18,6 +18,7 @@ public class GridVisitor {
     OutputCounter outputCounter = new OutputCounter(grid.getConverter());
     matrix.findAllPath(startingPoint, endPoint, new boolean[size * size], outputCounter);
     outputCounter.printSize();
+    // outputCounter.printPaths(); // uncomment to show paths
   }
 
   public static GraphMatrix createPossibleEdges(Grid grid) {
