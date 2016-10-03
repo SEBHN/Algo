523507357237d23cdcb997e6f520fd8410755c29
Manuel <wtfjoke@livenet.ch>
2016-10-03 12:32:22 +0200

    add loop for n

--- src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
+++ src/de/sebhn/algorithm/excercise1/helper/StarterHelper.java
@@ -5,14 +5,15 @@ import de.sebhn.algorithm.excercise1.OutputCounter;
 public class StarterHelper {
 
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    int n = 3;
-    Grid grid = new Grid(n);
-    System.out.println("n=" + n);
-    GraphMatrix matrix = GridVisitor.createPossibleEdges(grid);
-    OutputCounter out = GridVisitor.findPath(grid, matrix);
-    out.printSize();
-    // out.printPaths(); // uncomment to show paths
-    System.out.println("duration: " + (System.currentTimeMillis() - start));
+    for (int n = 0; n < 20; n++) {
+      long start = System.currentTimeMillis();
+      Grid grid = new Grid(n);
+      System.out.println("n=" + n);
+      GraphMatrix matrix = GridVisitor.createPossibleEdges(grid);
+      OutputCounter out = GridVisitor.findPath(grid, matrix);
+      out.printSize();
+      // out.printPaths(); // uncomment to show paths
+      System.out.println("duration: " + (System.currentTimeMillis() - start));
+    }
   }
 }
