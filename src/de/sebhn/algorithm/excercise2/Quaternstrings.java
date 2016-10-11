cd3fb47e8097f9ba4391c56ab3c33a51536dbcaa
Manuel <wtfjoke@livenet.ch>
2016-10-11 14:43:00 +0200

    approach which could work out

--- src/de/sebhn/algorithm/excercise2/Quaternstrings.java
+++ src/de/sebhn/algorithm/excercise2/Quaternstrings.java
@@ -1,33 +1,66 @@
 package de.sebhn.algorithm.excercise2;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Quaternstrings {
 
   public static void main(String[] args) {
-    Quaternstrings.calculate(3);
+    Quaternstrings.calculate(1000);
   }
 
   public static void calculate(int n) {
     int radix = 4;
-
-    List<String> possibleNumbers = new ArrayList<>();
     BigInteger maxNumber = new BigInteger("4").pow(n);
+    BigInteger counter = BigInteger.ZERO;
     System.out.println("maxnbr " + maxNumber);
 
     for (BigInteger i = BigInteger.ZERO; i.compareTo(maxNumber) < 0; i = i.add(BigInteger.ONE)) {
       String numberAsRadix4 = i.toString(radix);
-      while (numberAsRadix4.length() < n) {
-        numberAsRadix4 = "0" + numberAsRadix4;
+      numberAsRadix4 = addLeadingZeros(n, numberAsRadix4);
+
+      if (canBePrinted(numberAsRadix4)) {
+        counter = counter.add(BigInteger.ONE);
+        // System.out.println(numberAsRadix4);
       }
-      possibleNumbers.add(numberAsRadix4);
     }
 
-    for (String number : possibleNumbers) {
-      System.out.println(number);
+    System.out.println("Amount of possibleNumbers " + counter);
+  }
+
+  // TODO: make following code faster
+  private static boolean canBePrinted(String numberAsRadix4) {
+    boolean print = true;
+    char[] numbers = numberAsRadix4.toCharArray();
+    for (int j = 0; j < numbers.length; j++) {
+      char currentChar = numbers[j];
+      int nextIndex = j + 1;
+      if (hasNextIndex(numbers, nextIndex)) {
+        char nextChar = numbers[nextIndex];
+        int currentCharAsNumber = currentChar;
+        int nextCharAsNumber = nextChar;
+        if (nextCharAsNumber == currentCharAsNumber + 1) {
+          print = false;
+        }
+      }
     }
-    System.out.println("Amount of possibleNumbers " + possibleNumbers.size());
+    return print;
+  }
+
+  private static String addLeadingZeros(int length, String numberAsRadix4) {
+    while (numberAsRadix4.length() < length) {
+      numberAsRadix4 = "0" + numberAsRadix4;
+    }
+    return numberAsRadix4;
+  }
+
+  private static boolean hasNextIndex(char[] numbers, int nextIndex) {
+    try {
+      @SuppressWarnings("unused")
+      char nextChar = numbers[nextIndex];
+      return true;
+    } catch (ArrayIndexOutOfBoundsException aio) {
+      // ignore
+      return false;
+    }
   }
 }
