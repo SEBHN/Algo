c5f2feb5c88142acb4e345faa7bdb52114af475d
Phi Long Tran <philongtran@outlook.de>
2016-10-12 10:44:04 +0200

    add main class

---
+++ src/de/sebhn/algorithm/excercise3_3/fibonacci.java
@@ -0,0 +1,41 @@
+package de.sebhn.algorithm.excercise3_3;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+
+public class fibonacci {
+  static int betrag[] = {1, 3, 7, 31, 47};
+  static int n = betrag.length; // Anzahl Muenzen
+
+  static long w[][]; // Tabelle
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 37; i++) {
+      int g = F(i).intValue(); // g lesen
+      w = new long[g + 1][n]; // w dimensionieren
+      System.out.println(
+          "Den Betrag " + g + " kann man auf " + w(g, n - 1) + " verschiedene Arten wecheln.");
+    }
+  }
+
+  static HashMap<Integer, BigInteger> F = new HashMap<>(); // dynamic table
+
+  public static BigInteger F(int n) { // method
+    if (n <= 1) {
+      return BigInteger.valueOf(n);
+    } // base case
+    BigInteger m = F.get(n); // lookup
+    if (m != null)
+      return m; // sucessful
+    m = F(n - 1).add(F(n - 2)); // recursion
+    F.put(n, m); // store
+    return m; // finish
+  }
+
+
+  static long w(int g, int i) { // Methode
+    return g < 0 ? 0
+        : i == 0 ? (g % betrag[0] == 0 ? 1 : 0)
+            : w[g][i] != 0 ? w[g][i] : (w[g][i] = w(g, i - 1) + w(g - betrag[i], i));
+  }
+}
