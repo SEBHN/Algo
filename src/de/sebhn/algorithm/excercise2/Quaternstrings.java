5166e01be27eb883dbce012ebd6aa37ff74d3412
Manuel <wtfjoke@livenet.ch>
2016-10-12 12:24:03 +0200

    replace array with 4 bigintegers

--- src/de/sebhn/algorithm/excercise2/Quaternstrings.java
+++ src/de/sebhn/algorithm/excercise2/Quaternstrings.java
@@ -5,12 +5,14 @@ import java.math.BigInteger;
 public class Quaternstrings {
 
   private int n;
-  private BigInteger[] currentPairs;
+  private BigInteger a0;
+  private BigInteger a1;
+  private BigInteger a2;
+  private BigInteger a3;
 
   public Quaternstrings(int n) {
     this.n = n;
-    this.currentPairs =
-        new BigInteger[] {BigInteger.ONE, BigInteger.ONE, BigInteger.ONE, BigInteger.ONE};
+    a0 = a1 = a2 = a3 = BigInteger.ONE;
     System.out.println("n=" + n);
   }
 
@@ -32,29 +34,19 @@ public class Quaternstrings {
   }
 
   private BigInteger getAmountOfPaths() {
-    BigInteger a0 = currentPairs[0];
-    BigInteger a1 = currentPairs[1];
-    BigInteger a2 = currentPairs[2];
-    BigInteger a3 = currentPairs[3];
-
     BigInteger amountOfPaths = a0.add(a1).add(a2).add(a3);
     return amountOfPaths;
   }
 
   private void addInPlace() {
-    BigInteger a0 = currentPairs[0];
-    BigInteger a1 = currentPairs[1];
-    BigInteger a2 = currentPairs[2];
-    BigInteger a3 = currentPairs[3];
-
     BigInteger newa0 = a0.add(a2).add(a3); // exclude 1
     BigInteger newa1 = a1.add(a0).add(a3); // exclude 2
     BigInteger newa2 = a2.add(a0).add(a1); // exclude 3
     BigInteger newa3 = a3.add(a0).add(a1).add(a2); // every number allowed
 
-    currentPairs[0] = newa0;
-    currentPairs[1] = newa1;
-    currentPairs[2] = newa2;
-    currentPairs[3] = newa3;
+    a0 = newa0;
+    a1 = newa1;
+    a2 = newa2;
+    a3 = newa3;
   }
 }
