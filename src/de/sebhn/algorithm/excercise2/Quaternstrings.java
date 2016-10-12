5e91841bf7b9b4a699778ca637d7639d7b5189ec
Manuel <wtfjoke@livenet.ch>
2016-10-12 11:12:05 +0200

    implement cool algorithm :)

--- src/de/sebhn/algorithm/excercise2/Quaternstrings.java
+++ src/de/sebhn/algorithm/excercise2/Quaternstrings.java
@@ -1,76 +1,60 @@
 package de.sebhn.algorithm.excercise2;
 
-import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Quaternstrings {
 
-  public static void main(String[] args) throws Exception {
-    // Quaternstrings.calculate(3);
-    Quaternstrings.writeFileWithAllNumbers(30);
-  }
+  private int n;
+  private BigInteger[] currentPairs;
 
-  public static void writeFileWithAllNumbers(int n) throws IOException {
-    int radix = 4;
-    BigInteger maxNumber = new BigInteger("4").pow(n);
-    Path path = Paths.get("C:\\Temp\\numbers.txt");
-    for (BigInteger i = BigInteger.ZERO; i.compareTo(maxNumber) < 0; i = i.add(BigInteger.ONE)) {
-      String numberAsRadix4 = i.toString(radix);
-      Files.write(path, numberAsRadix4.getBytes());
-    }
-  }
-
-  public static void calculate(int n) {
-    int radix = 4;
-    BigInteger maxNumber = new BigInteger("4").pow(n);
-    BigInteger counter = BigInteger.ZERO;
+  public Quaternstrings(int n) {
+    this.n = n;
+    this.currentPairs =
+        new BigInteger[] {BigInteger.ONE, BigInteger.ONE, BigInteger.ONE, BigInteger.ONE};
     System.out.println("n=" + n);
-    System.out.println("maxnbr " + maxNumber);
-
-    for (BigInteger i = BigInteger.ZERO; i.compareTo(maxNumber) < 0; i = i.add(BigInteger.ONE)) {
-      String numberAsRadix4 = i.toString(radix);
-      numberAsRadix4 = addLeadingZeros(n, numberAsRadix4);
-      if (canBePrinted(numberAsRadix4)) {
-        counter = counter.add(BigInteger.ONE);
-        // System.out.println(numberAsRadix4);
-      }
-    }
-
-    System.out.println("Amount of possibleNumbers " + counter);
   }
 
-  private static String addLeadingZeros(int length, String numberAsRadix4) {
-    while (numberAsRadix4.length() < length) {
-      numberAsRadix4 = "0" + numberAsRadix4;
-    }
-    return numberAsRadix4;
+  public static void main(String[] args) throws Exception {
+    Quaternstrings.calculate(2);
   }
 
-  // TODO: make following code faster
-  private static boolean canBePrinted(String numberAsRadix4) {
-    boolean print = true;
-    char[] numbers = numberAsRadix4.toCharArray();
-
-    for (int j = 0; j < numbers.length; j++) {
-      char currentChar = numbers[j];
-      int nextIndex = j + 1;
-      if (hasNextIndex(numbers, nextIndex)) {
-        char nextChar = numbers[nextIndex];
-        int currentCharAsNumberPlusOne = Character.getNumericValue(currentChar) + 1;
-        int nextCharAsNumber = Character.getNumericValue(nextChar);
-        if (nextCharAsNumber == currentCharAsNumberPlusOne) {
-          print = false;
-          break;
-        }
-      }
-    }
-    return print;
+  public static BigInteger calculate(int n) {
+    return new Quaternstrings(n).calculate();
   }
 
-  private static boolean hasNextIndex(char[] numbers, int nextIndex) {
-    return nextIndex < numbers.length;
+  private BigInteger calculate() {
+    for (int i = 1; i < n; i++) {
+      addInPlace();
+    }
+    BigInteger amountOfPaths = getAmountOfPaths();
+    System.out.println("Amount of paths: " + amountOfPaths);
+    return amountOfPaths;
+  }
+
+  private BigInteger getAmountOfPaths() {
+    BigInteger a0 = currentPairs[0];
+    BigInteger a1 = currentPairs[1];
+    BigInteger a2 = currentPairs[2];
+    BigInteger a3 = currentPairs[3];
+
+    BigInteger amountOfPaths = a0.add(a1).add(a2).add(a3);
+    return amountOfPaths;
+  }
+
+  private void addInPlace() {
+    BigInteger a0 = currentPairs[0];
+    BigInteger a1 = currentPairs[1];
+    BigInteger a2 = currentPairs[2];
+    BigInteger a3 = currentPairs[3];
+
+    BigInteger newa0 = a0.add(a2).add(a3); // exclude 1
+    BigInteger newa1 = a1.add(a0).add(a3); // exclude 2
+    BigInteger newa2 = a2.add(a0).add(a1); // exclude 3
+    BigInteger newa3 = a3.add(a0).add(a1).add(a2); // every number allowed
+
+    currentPairs[0] = newa0;
+    currentPairs[1] = newa1;
+    currentPairs[2] = newa2;
+    currentPairs[3] = newa3;
   }
 }
--- src_test/de/sebhn/algorithm/excercise2/QuaternstringsTest.java
+++ src_test/de/sebhn/algorithm/excercise2/QuaternstringsTest.java
@@ -2,7 +2,6 @@ package de.sebhn.algorithm.excercise2;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -10,52 +9,52 @@ public class QuaternstringsTest {
 
   @Test
   public void testN1() throws Exception {
-    Quaternstrings.calculate(1);
+    assertEquals("4", Quaternstrings.calculate(1).toString());
   }
 
   @Test
   public void testN2() throws Exception {
-    Quaternstrings.calculate(2);
+    assertEquals("13", Quaternstrings.calculate(2).toString());
   }
 
   @Test
   public void testN3() throws Exception {
-    Quaternstrings.calculate(3);
+    assertEquals("42", Quaternstrings.calculate(3).toString());
   }
 
   @Test
   public void testN4() throws Exception {
-    Quaternstrings.calculate(4);
+    assertEquals("136", Quaternstrings.calculate(4).toString());
   }
 
   @Test
   public void testN5() throws Exception {
-    Quaternstrings.calculate(5);
+    assertEquals("440", Quaternstrings.calculate(5).toString());
   }
 
   @Test
   public void testN6() throws Exception {
-    Quaternstrings.calculate(6);
+    assertEquals("1423", Quaternstrings.calculate(6).toString());
   }
 
   @Test
   public void testN7() throws Exception {
-    Quaternstrings.calculate(7);
+    assertEquals("4602", Quaternstrings.calculate(7).toString());
   }
 
   @Test
   public void testN8() throws Exception {
-    Quaternstrings.calculate(8);
+    assertEquals("14883", Quaternstrings.calculate(8).toString());
   }
 
   @Test
   public void testN9() throws Exception {
-    Quaternstrings.calculate(9);
+    assertEquals("48132", Quaternstrings.calculate(9).toString());
   }
 
   @Test
   public void testN10() throws Exception {
-    Quaternstrings.calculate(10);
+    assertEquals("155660", Quaternstrings.calculate(10).toString());
   }
 
   /**
@@ -64,16 +63,10 @@ public class QuaternstringsTest {
    * @throws Exception
    */
   @Test
-  @Ignore
   public void testN1000() throws Exception {
-    Quaternstrings.calculate(1000);
-  }
-
-  @Test
-  public void testCharArray() throws Exception {
-    String anumber = "001";
-    char[] charArray = anumber.toCharArray();
-    assertEquals(3, charArray.length);
+    assertEquals(
+        "688392248037533757713764205706571705220243732319430179416242107760492969167450259416609386077991322671808671360568171761092554091087989909471229125400396713067129246831063231667097668213892255075339331792216099438420916353841538065913791571915375840112744147104775438060876515976193042885118501910610484400419840166574866304901830040616116115132077025726575992800701172374400231947975641504497518075894990138077106917270967959170682665660707079805188366662268687902881468825906809774748357181873845140529093548",
+        Quaternstrings.calculate(1000).toString());
   }
 
 }
