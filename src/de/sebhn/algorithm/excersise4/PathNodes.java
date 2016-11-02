537d2097ae283eb33cbbcb11313816de86bfea3d
Manuel <wtfjoke@livenet.ch>
2016-11-02 10:51:01 +0100

    add sumOfPath and beautify code

--- src/de/sebhn/algorithm/excersise4/PathNodes.java
+++ src/de/sebhn/algorithm/excersise4/PathNodes.java
@@ -7,39 +7,44 @@ import java.math.BigInteger;
  */
 public class PathNodes {
 
+  private static int sumOfPaths;
+
   public static void main(String[] args) {
-    int n = 2;
-    BigInteger numberOfPaths = b(n, n, 0, "");
+    int n = 1;
+    sumOfPaths = 0;
+    BigInteger numberOfPaths = calculatePaths(n, n, 0, "");
     System.out.println(numberOfPaths);
+    System.out.println("sum: " + sumOfPaths);
   }
 
-  private static BigInteger b(int x, int y, int t, String s) {
+  private static BigInteger calculatePaths(int x, int y, int step, String path) {
     if (x < 0 || y < 0) {
       return BigInteger.ZERO;
     } else if (x == 0 && y == 0) {
-      System.out.println(" " + s + "."); // print path
+      sumOfPaths += path.length() + 1;
+      System.out.println(" " + path + "."); // print path
       return BigInteger.ONE;
     }
 
-    BigInteger r = BigInteger.ZERO;
+    BigInteger numberOfPaths = BigInteger.ZERO;
 
     if (y < x) {
-      r = r.add(b(x - 1, y, 0, "R" + s)); // right
+      numberOfPaths = numberOfPaths.add(calculatePaths(x - 1, y, 0, "R" + path)); // right
     }
     if (y <= x) {
-      r = r.add(b(x, y - 1, 0, "U" + s)); // up
+      numberOfPaths = numberOfPaths.add(calculatePaths(x, y - 1, 0, "U" + path)); // up
     }
     if (y >= x) {
-      r = r.add(b(x - 1, y - 1, 0, "F" + s)); // upRight
+      numberOfPaths = numberOfPaths.add(calculatePaths(x - 1, y - 1, 0, "F" + path)); // upRight
     }
-    if (y > x + 1 && t != 2) {
-      r = r.add(b(x + 1, y - 1, 1, "L" + s)); // upLeft
+    if (y > x + 1 && step != 2) {
+      numberOfPaths = numberOfPaths.add(calculatePaths(x + 1, y - 1, 1, "L" + path)); // upLeft
     }
-    if (y >= x && t != 1) {
-      r = r.add(b(x - 1, y + 1, 2, "D" + s)); // downRight
+    if (y >= x && step != 1) {
+      numberOfPaths = numberOfPaths.add(calculatePaths(x - 1, y + 1, 2, "D" + path)); // downRight
     }
 
-    return r;
+    return numberOfPaths;
   }
 
 }
